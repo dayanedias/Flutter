705ce8d7b29403c1e532cd4df2fcef7aaab99b23
Dayane Rodrigues <dayane.rodrigues@softplan.com.br>
2020-08-05 14:14:37 -0300

    Complete app

--- lib/ui/home_page.dart
+++ lib/ui/home_page.dart
@@ -1,7 +1,11 @@
 import 'dart:convert';
 
+import 'package:buscador_giphy/ui/gif_page.dart';
+import 'package:buscador_giphy/ui/gif_page.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
+import 'package:share/share.dart';
+import 'package:transparent_image/transparent_image.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -16,7 +20,7 @@ class _HomePageState extends State<HomePage> {
   Future<Map> _getGifs() async {
     http.Response response;
 
-    if(_search == null)
+    if(_search == null || _search.isEmpty)
       response = await http.get("https://api.giphy.com/v1/gifs/trending?api_key=OwFjASx4ktnHBhX6nTQXUiBgn8KFTHYj&limit=20&rating=g");
     else
       response = await http.get("https://api.giphy.com/v1/gifs/search?api_key=OwFjASx4ktnHBhX6nTQXUiBgn8KFTHYj&q=$_search&limit=19&offset=$_offset&rating=g&lang=pt");
@@ -110,9 +114,18 @@ class _HomePageState extends State<HomePage> {
         itemBuilder: (context, index) {
         if(_search == null || index < snapshot.data["data"].length)
           return GestureDetector(
-            child: Image.network(snapshot.data["data"][index]["images"]["fixed_height"]["url"],
+            child: FadeInImage.memoryNetwork(placeholder: kTransparentImage,
+              image: snapshot.data["data"][index]["images"]["fixed_height"]["url"],
               height: 300.0,
-              fit: BoxFit.cover,)
+              fit: BoxFit.cover,
+            ),
+            onTap: () => {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => GifPage(snapshot.data["data"][index]))),
+            },
+            onLongPress: () {
+              Share.share(snapshot.data["data"][index]["images"]["fixed_height"]["url"]);
+            },
           );
           else
             return Container(
